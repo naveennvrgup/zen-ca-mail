2a24580a3a31b65a41e7c209f2a9f9bd0d41c642
naveennvrgup <naveennvrgup@gmail.com>
2019-01-23 12:23:28 +0530

    configured mail

---
+++ backend/boto3_mail/client_cloudfront.py
@@ -0,0 +1,9 @@
+import boto3
+from decouple import config
+
+client = boto3.client(
+    'cloudwatch',
+    region_name=config('region_name'),
+    aws_access_key_id=config('aws_access_key_id'),
+    aws_secret_access_key=config('aws_secret_access_key')
+)
