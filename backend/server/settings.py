af0e885fb5712ac622b7ab7b90cfdba863eae0ee
naveennvrgup <naveennvrgup@gmail.com>
2019-01-26 21:51:43 +0530

    pagination

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 100,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
