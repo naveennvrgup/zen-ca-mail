8454cb682ef9a7ab3e1e2d73f4d564a553d097ff
naveennvrgup <naveennvrgup@gmail.com>
2019-01-26 21:26:06 +0530

    build

---
+++ frontend/build/precache-manifest.bfa2b991f65e14843dabf589d0b7ac0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f259361c5c6356bc2ea83942c9fe9869",
+    "url": "/static/media/logo.f259361c.png"
+  },
+  {
+    "revision": "3a9eda7d3a69b0305c564468f6909d9f",
+    "url": "/static/media/home-img.3a9eda7d.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cc449b2c9d7890a30861",
+    "url": "/static/js/main.cc449b2c.chunk.js"
+  },
+  {
+    "revision": "91fbcc828a20dfad2361",
+    "url": "/static/js/1.91fbcc82.chunk.js"
+  },
+  {
+    "revision": "cc449b2c9d7890a30861",
+    "url": "/static/css/main.654f3c0b.chunk.css"
+  },
+  {
+    "revision": "91fbcc828a20dfad2361",
+    "url": "/static/css/1.f169ee3d.chunk.css"
+  },
+  {
+    "revision": "a0b8ef5ca44a5fe6987911b4c0a1d20f",
+    "url": "/index.html"
+  }
+];
