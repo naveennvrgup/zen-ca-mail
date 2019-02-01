237102aeb72bf3ba7554a9705524263adc66595c
naveennvrgup <naveennvrgup@gmail.com>
2019-02-01 17:02:22 +0530

    removed unwanted app

--- backend/outbox/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Outbox)
-admin.site.register(Batch)
--- backend/sent/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SentConfig(AppConfig):
-    name = 'sent'
