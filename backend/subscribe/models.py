7d97c543f872729ce9f88e67d57058630fa2bde2
naveennvrgup <naveennvrgup@gmail.com>
2019-02-01 15:02:57 +0530

    new sub model

--- backend/subscribe/models.py
+++ backend/subscribe/models.py
@@ -8,7 +8,6 @@ class Subscriber(Model):
     email = EmailField()
     mobile = CharField(max_length=100, default='')
     verified = BooleanField(default=False)
-    bounces = IntegerField(default=0)
     otp = CharField(max_length=5, default='00000')
     created_on = DateTimeField(auto_now_add=True)
     flag = BooleanField(default=False)
@@ -19,7 +18,7 @@ class Subscriber(Model):
 
 class Group(Model):
     name = CharField(max_length=200, unique=True)
-    subs = ManyToManyField(Subscriber, related_name='groups')
+    subs = ForeignKey(Subscriber, on_delete=CASCADE, related_name='groups')
     flag = BooleanField(default=False)
     created_on = DateTimeField(auto_now_add=True)
 
@@ -40,19 +39,3 @@ class GroupSerializer(ModelSerializer):
         model = Group
         fields = '__all__'
 
-# save all new subscribers in group all
-
-
-@receiver(signals.post_save, sender=Subscriber)
-def pre_save_outbox(sender, instance, **kwargs):
-    Group.objects.get(name='all').subs.add(instance)
-
-
-# create group all if not already exists
-# try:
-#     all = Group.objects.get(name='all')
-#     if all:
-#         print('group all exists')
-# except:
-#     all = Group.objects.create(name='all')
-#     print('created group all')
