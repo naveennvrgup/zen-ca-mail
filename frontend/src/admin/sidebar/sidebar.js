a761861f4d06599fc4b16bc8f170f142a2447c8d
naveennvrgup <naveennvrgup@gmail.com>
2019-02-18 15:10:53 +0530

    sidebar done

--- frontend/src/admin/sidebar/sidebar.js
+++ frontend/src/admin/sidebar/sidebar.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react'
 import { NavLink, withRouter } from 'react-router-dom'
 
 class sidebar extends Component {
+    state = {
+        sbopen: false
+    }
+
+    toggle_sidebar = (e) => {
+        this.setState({
+            ...this.state,
+            sbopen: !this.state.sbopen
+        })// end of setstate
+    }
+
     render() {
         return (
-            <div className='sidebar'>
-                <div className=" pt-3 px-3 text-right">
-                    <button className="btn close-sidebar-btn">
+            <div className={`sidebar ${this.state.sbopen? 'sidebar-open': ''}`}>
+                <div className="text-right">
+                    <div
+                        onClick={this.toggle_sidebar}
+                        className="btn close-sidebar-btn">
                         <i className="fa fa-bars fa-2x p-0"></i>
-                    </button>
+                    </div>
                 </div>
                 <div className="contents pt-3">
                     <div className="admin-logo text-center ">
@@ -16,23 +29,23 @@ class sidebar extends Component {
                     </div>
                     {/* <h5 className=' text-white mt-3'>Admin</h5> */}
                     <div className='links mt-4'>
-                        <NavLink to='/admin/dashboard/' className="link ">
+                        <NavLink onClick={this.toggle_sidebar} to='/admin/dashboard/' className="link ">
                             <i className="far fa-chart-bar"></i>
                             <span>Dashboard</span>
                         </NavLink>
-                        <NavLink to='/admin/group/' className="link ">
+                        <NavLink onClick={this.toggle_sidebar} to='/admin/group/' className="link ">
                             <i className="fa fa-users"></i>
                             <span to='/admin/group'>Subscribers</span>
                         </NavLink>
-                        <NavLink to='/admin/email/' className="link ">
+                        <NavLink onClick={this.toggle_sidebar} to='/admin/email/' className="link ">
                             <i className="far fa-envelope"></i>
                             <span to='/admin/email'>E-mail</span>
                         </NavLink>
-                        <NavLink to='/admin/news/' className="link ">
+                        <NavLink onClick={this.toggle_sidebar} to='/admin/news/' className="link ">
                             <i className="far fa-newspaper"></i>
                             <span to='/admin/news'>Newsfeed</span>
                         </NavLink>
-                        <NavLink to='/admin/Signout/' className="link ">
+                        <NavLink onClick={this.toggle_sidebar} to='/admin/Signout/' className="link ">
                             <i className="fas fa-sign-out-alt"></i>
                             <span to='/admin/signout'>Signout</span>
                         </NavLink>
