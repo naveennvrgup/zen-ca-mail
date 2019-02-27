da19629e5ec3b916ee60955ead4f6bafe1ebc2f0
naveennvrgup <naveennvrgup@gmail.com>
2019-02-27 17:09:50 +0530

    started with contact us validation and api

--- frontend/src/main/contactus.js
+++ frontend/src/main/contactus.js
@@ -1,6 +1,54 @@
 import React, { Component } from 'react'
 
 export default class contactus extends Component {
+  Error = () => <div className="my-3 text-danger">{this.state.error}</div>
+  Success = () => <div className='my-3'>{this.state.success}</div>
+
+  componentDidMount = () => {
+    this.form = document.querySelector('.contactus form')
+    this.name = this.form.querySelector('#name')
+    this.email = this.form.querySelector('#email')
+    this.mobile = this.form.querySelector('#phone')
+    this.msg = this.form.querySelector('#msg')
+  }
+
+
+  place_enquiry_handler = e => {
+    e.preventDefault()
+    console.log(this.form);
+    return
+
+    if (!this.form.checkValidity()) {
+      this.setState({
+        ...this.state,
+        error: 'Please fill all the fields with valid details!'
+      })// end of setstate
+      return
+    }
+
+    this.axios.post('/api/sub_from_main/', {
+      'name': this.name.value,
+      'email': this.email.value,
+      'mobile': this.mobile.value
+    }).then(d => {
+      d = d.data
+      console.log(d)
+      if (d.error) {
+        this.setState({
+          ...this.state,
+          error: d.error,
+          success: null,
+        })// end of setstate
+      } else {
+        this.setState({
+          ...this.state,
+          success: d.msg,
+          error: null,
+        })// end of setstate
+      }
+    })
+  }
+
   render() {
     return (
       <div className='contactus'>
@@ -15,14 +63,16 @@ export default class contactus extends Component {
               </p>
               <form className='mt-4'>
                 <input type="text" id="name" placeholder="Name" />
-                <input type="text" id="name" placeholder="Phone" />
-                <input type="text" id="name" placeholder="Email" />
-                <textarea rows='3' type="text" id="name" placeholder="Your message" />
-                <button id="enquiry-btn" className='mt-4'>
+                <input type="text" id="phone" placeholder="Phone" />
+                <input type="text" id="email" placeholder="Email" />
+                <textarea rows='3' type="text" id="msg" placeholder="Your message" />
+                <button onClick={this.place_enquiry_handler} id="enquiry-btn" className='mt-4'>
                   Send
                 </button>
               </form>
             </div>
+
+            {/* right side */}
             <div className="col-md-6 right text-center">
               <h2 className="text-center hf">Contact</h2>
               <p className="mt-2">Have a question? We can help you...</p>
@@ -62,7 +112,7 @@ export default class contactus extends Component {
                   <div className="social">
                     <i className="fab mx-2 fa-facebook"></i>
                     <i className="fab mx-2 fa-twitter"></i>
-                    <i className="fab mx-2 fa-linkedin"></i> 
+                    <i className="fab mx-2 fa-linkedin"></i>
                   </div>
                 </div>
               </div>
