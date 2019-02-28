8eaa2062eb1b4894bd76270e55b9bb5238a8f616
naveennvrgup <naveennvrgup@gmail.com>
2019-02-28 10:27:25 +0530

    news nearly done

--- frontend/src/main/news.js
+++ frontend/src/main/news.js
@@ -10,18 +10,38 @@ export default class SimpleSlider extends Component {
     axios = fuser()
 
     componentDidMount = () => {
+        this.slider = document.querySelector('.slider')
+        this.mouse_on_slider = false
+
+        this.slider.addEventListener('mouseover', () => {
+            this.mouse_on_slider = true
+            // console.log('mouse over');
+        })
+
+        this.slider.addEventListener('mouseout', () => {
+            this.mouse_on_slider = false
+            // console.log('mouse out');
+        })
+
         this.axios.get('/api/get_news/').then(d => {
             d = d.data
+            d = d.map(ele => this.html_news(ele))
             console.log(d)
             this.setState({
                 ...this.state,
                 news: d
             })// end of setstate
         })
+
+        setInterval(() => {
+            if(this.mouse_on_slider){
+                return
+            }
+            this.change_news_handler({}, 1)
+        }, 3000);
     }
 
-    curr_news = (snid) => {
-        const ele = this.state.news[snid]
+    html_news = ele => {
         let temp_img = require('../assets/news_placeholder.jpeg')
 
         if (ele.img) {
@@ -29,34 +49,61 @@ export default class SimpleSlider extends Component {
         }
 
         ele.img = temp_img
-
-        return (
-            <div className="slider mt-4">
-                <div className='img'>
-                    <img src={ele.img} alt="" />
-                </div>
-                <div className='info'>
-                    <h4 className='hf'>
-                        <span>{ele.title}</span>
-                        <span>
-                            <a href={ele.link} id='slider-link'>link</a>
-                        </span>
-                    </h4>
-                    <p className="mt-4">
-                        {ele.brief}
-                    </p>
-                </div>
+        return <div className="wrapper">
+            <div className='img'>
+                <img src={ele.img} alt="" />
             </div>
-        )
+            <div className='info'>
+                <h4 className='hf'>
+                    <span>{ele.title}</span>
+                    <span>
+                        {ele.link ? <a
+                            rel="noopener noreferrer"
+                            target='_blank'
+                            href={ele.link} id='slider-link'>link</a> : ''}
+                    </span>
+                </h4>
+                <p className="mt-4">
+                    {ele.brief}
+                </p>
+            </div>
+        </div>
+    }
+
+    change_news_handler = (e, inc) => {
+        let len = this.state.news.length
+        let new_snid = (this.state.snid + inc) % len
+        if (new_snid < 0) {
+            new_snid = len - 1
+        }
+        console.log(new_snid);
+        this.setState({
+            ...this.state,
+            snid: new_snid
+        })// end of setstate
     }
 
     render() {
 
         return (
-            this.state.news.length ? <div className='news_main'>
+            <div className='news_main'>
                 <h2 className="text-center hf">Newsfeed</h2>
-                {this.curr_news(this.state.snid)}
-            </div> : ''
+                <div className="slider mt-4">
+                    {this.state.news[this.state.snid]}
+                    <div className="controls mt-3">
+                        <button
+                            onClick={e => this.change_news_handler(e, -1)}
+                            className='btn nbtn'>
+                            <i className="fa fa-angle-left"></i>
+                        </button>
+                        <button
+                            onClick={e => this.change_news_handler(e, 1)}
+                            className='btn nbtn'>
+                            <i className="fa fa-angle-right"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
