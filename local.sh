b422dd64ddf1e094aee0f7a182d68de3ae8db0d3
deep-matelabs <deep-matelabs@github.com>
2019-06-24 17:29:36 +0530

    built

--- local.sh
+++ local.sh
@@ -3,6 +3,5 @@ cd frontend
 npm run build
 cd ../backend
 source env/bin/activate
-python3 manage.py migrate
 python3 manage.py collectstatic --no-input
-python3 manage.py celery
+python3 manage.py runserver
