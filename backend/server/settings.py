7a5f9ff2f80e7f2c97293767d5e0278d02937144
naveennvrgup <naveennvrgup@gmail.com>
2020-02-08 12:15:00 +0530

    commented session auth

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -119,8 +119,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 15,
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
